f3a95dbb8e407a6c1c5999b2b072c14e7d6ae820
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-12 15:00:41 -0500

    feat: Add /new get and post requests

--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -1,7 +1,13 @@
 import Router from "express";
-import { categoryPath } from "../controllers/categoryController.js";
+import { categoryPath, newItemFormPath } from "../controllers/categoryController.js";
 
 const categoryRouter = Router();
+
+categoryRouter.post("/new", newItemFormPath());
+
+categoryRouter.get("/new", (req, res) => {
+    res.render("newItem", {title: "New Item"})
+});
 
 categoryRouter.get("/:category", categoryPath);
 
