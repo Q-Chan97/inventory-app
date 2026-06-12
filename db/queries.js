c574fca746710657be04d6d7a786130434b773b2
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-12 15:00:18 -0500

    feat: New item and cat name

--- db/queries.js
+++ db/queries.js
@@ -4,3 +4,12 @@ export async function selectInventoryFromCategory(category) {
     const { rows } = await pool.query('SELECT inventory.* FROM inventory JOIN categories ON inventory.category_id = categories.id WHERE categories.category = ($1)', [category]);
     return rows;
 }
+
+export async function getCategoryNameById(id) {
+    const result = await pool.query('SELECT category FROM categories WHERE id = $1', [id]);
+    return result.rows[0].category; // Returns string from object
+}
+
+export async function addNewItem(category, itemName, quantity, rating, notes, proof) {
+    await pool.query('INSERT INTO inventory (category_id, name, quantity, rating, notes, proof) VALUES ($1, $2, $3, $4, $5, $6)', [category, itemName, quantity, rating, notes, proof])
+}
