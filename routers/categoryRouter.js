9d25959f4332e9eaec128e1a928573e38bdf92f4
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-15 17:49:17 -0500

    feat: Add routes for delete

--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -1,5 +1,5 @@
 import Router from "express";
-import { categoryPath, getDetails, newItemFormPath, updateItemFormPath } from "../controllers/categoryController.js";
+import { categoryPath, deleteItemPath, getDetails, newItemFormPath, updateItemFormPath } from "../controllers/categoryController.js";
 import { findItem, getCategoryNameById } from "../db/queries.js";
 
 const categoryRouter = Router();
@@ -10,7 +10,7 @@ categoryRouter.get("/new", (req, res) => {
     res.render("newItem", {title: "New Item"})
 });
 
-categoryRouter.post("/:category/:itemId/update", updateItemFormPath())
+categoryRouter.post("/:category/:itemId/update", updateItemFormPath());
 categoryRouter.get("/:category/:itemId/update", async (req, res) => {
     const { itemId } = req.params;
 
@@ -18,7 +18,17 @@ categoryRouter.get("/:category/:itemId/update", async (req, res) => {
     const catLink = await getCategoryNameById(item.category_id)
 
     res.render("updateItem", {title: "Update Item", item: item, catLink: catLink});
-})
+});
+
+categoryRouter.post("/:category/:itemId/delete", deleteItemPath);
+categoryRouter.get("/:category/:itemId/delete", async (req, res) => {
+    const { itemId } = req.params;
+
+    const item = await findItem(itemId);
+    const catLink = await getCategoryNameById(item.category_id);
+
+    res.render("deleteView", {title: "Delete Item?", item: item, catLink: catLink})
+});
 
 categoryRouter.get("/:category/:itemId", getDetails);
 categoryRouter.get("/:category", categoryPath);
