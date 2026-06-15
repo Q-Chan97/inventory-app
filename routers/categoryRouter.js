12d376bec07629f5f2512a85385ec93070184565
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-06-15 16:45:10 -0500

    feat: Build update routes

--- routers/categoryRouter.js
+++ routers/categoryRouter.js
@@ -1,5 +1,6 @@
 import Router from "express";
-import { categoryPath, getDetails, newItemFormPath } from "../controllers/categoryController.js";
+import { categoryPath, getDetails, newItemFormPath, updateItemFormPath } from "../controllers/categoryController.js";
+import { findItem, getCategoryNameById } from "../db/queries.js";
 
 const categoryRouter = Router();
 
@@ -8,6 +9,16 @@ categoryRouter.post("/new", newItemFormPath());
 categoryRouter.get("/new", (req, res) => {
     res.render("newItem", {title: "New Item"})
 });
+
+categoryRouter.post("/:category/:itemId/update", updateItemFormPath())
+categoryRouter.get("/:category/:itemId/update", async (req, res) => {
+    const { itemId } = req.params;
+
+    const item = await findItem(itemId);
+    const catLink = await getCategoryNameById(item.category_id)
+
+    res.render("updateItem", {title: "Update Item", item: item, catLink: catLink});
+})
 
 categoryRouter.get("/:category/:itemId", getDetails);
 categoryRouter.get("/:category", categoryPath);
